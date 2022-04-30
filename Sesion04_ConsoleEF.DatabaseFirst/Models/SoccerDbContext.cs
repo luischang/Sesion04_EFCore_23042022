a7ca0d48f6ce4de390c1e1a0cd41997d43f10898
Luis Chang <luis.chang@outlook.com>
2022-04-30 09:35:42 -0500

    CRUD in SoccerDB

--- Sesion04_ConsoleEF.DatabaseFirst/Models/SoccerDbContext.cs
+++ Sesion04_ConsoleEF.DatabaseFirst/Models/SoccerDbContext.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.Logging;
 
 namespace Sesion04_ConsoleEF.DatabaseFirst.Models
 {
@@ -25,7 +26,7 @@ namespace Sesion04_ConsoleEF.DatabaseFirst.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=DESKTOP-S1DROK0\\SQLEXPRESS;Database=SoccerDb;Trusted_Connection=True;");
+                optionsBuilder.UseSqlServer("Server=DESKTOP-S1DROK0\\SQLEXPRESS;Database=SoccerDb;Trusted_Connection=True;").LogTo(Console.WriteLine, LogLevel.Information);
             }
         }
 
--- Sesion04_ConsoleEF.DatabaseFirst/Program.cs
+++ Sesion04_ConsoleEF.DatabaseFirst/Program.cs
@@ -6,13 +6,134 @@ Console.WriteLine("Hello, World!");
 
 var context = new SoccerDbContext();
 //Insert player
-var player = new Player()
+//var player = new Player()
+//{
+//    FullName = "Messi",
+//    Age = 34,
+//    Dorsal=10    
+//};
+
+//context.Add(player);
+//context.SaveChanges();
+
+//var player2 = new Player()
+//{
+//    FullName = "Cristiano Ronaldo",
+//    Age = 36,
+//    Dorsal = 7
+//};
+
+//var player3 = new Player()
+//{
+//    FullName = "Paolo Guerrero",
+//    Age = 38,
+//    Dorsal = 9
+//};
+
+//var player4 = new Player()
+//{
+//    FullName = "Gianluca Lapadula",
+//    Age = 32,
+//    Dorsal = 14
+//};
+
+//var playerList = new List<Player>();
+//playerList.Add(player2);
+//playerList.Add(player3);
+//playerList.Add(player4);
+
+////Insert All in SoccerDbContext
+//context.AddRange(playerList);
+//context.SaveChanges();
+
+//Remove Player
+//var playerToRemove2 = context.Player.Where(x => x.Id == 4).FirstOrDefault();
+// to Lambda expressions
+//var playerToRemove = context
+//                .Player
+//                .FirstOrDefault(p => p.Id == 4);
+//context.Player.Remove(playerToRemove);
+//context.SaveChanges();
+//LINQ query expressions
+//var playerToRemove2 = (from p in context.Player
+//                       where p.Id == 4
+//                       select p).FirstOrDefault();
+
+//context.Player.Remove(playerToRemove2);
+//context.SaveChanges();
+
+//Query with Lambda expressions
+
+var playerAll = context.Player.ToList();
+
+//Print in console
+foreach (var item in playerAll)
 {
-    FullName = "Messi",
-    Age = 34,
-    Dorsal=10    
-};
+    Console.WriteLine($"{item.FullName} - {item.Age} - {item.Dorsal}");
+    //Console.WriteLine("Fullname" + item.FullName)
+}
 
-context.Add(player);
+//Insert a new team
+//var team = new Team()
+//{
+//    Description = "Barcelona",
+//    Country = "ESPAÑA"
+//};
+
+//var team2 = new Team()
+//{
+//    Description = "Real Madrid",
+//    Country = "ESPAÑA"
+//};
+
+//var team3 = new Team()
+//{
+//    Description = "PSG",
+//    Country = "FRANCIA"
+//};
+
+////Create a list of teams
+//var teamList = new List<Team>();
+//teamList.Add(team);
+//teamList.Add(team2);
+//teamList.Add(team3);
+
+//Insert all in SoccerDbContext
+//context.Team.AddRange(teamList);
+//context.SaveChanges();
+
+//Create PlayerTeam
+//var playerTeam = new PlayerTeam()
+//{
+//    PlayerId = 1,
+//    TeamId = 1,
+//    InitalDate = DateTime.Now,
+//};
+
+//var playerTeam2 = new PlayerTeam()
+//{
+//    PlayerId = 2,
+//    TeamId = 2,
+//    InitalDate = DateTime.Now,
+//};
+
+//var playerTeam3 = new PlayerTeam()
+//{
+//    PlayerId = 3,
+//    TeamId = 3,
+//    InitalDate = DateTime.Now,
+//};
+
+////Create a list of PlayerTeam
+//var playerTeamList = new List<PlayerTeam>();
+//playerTeamList.Add(playerTeam);
+//playerTeamList.Add(playerTeam2);
+//playerTeamList.Add(playerTeam3);
+////Insert all in SoccerDbContext
+//context.PlayerTeam.AddRange(playerTeamList);
+//context.SaveChanges();
+
+//Search in player
+var player = context.Player.Where(x => x.Id == 3).FirstOrDefault();
+player.Age = 37;
 context.SaveChanges();
-
